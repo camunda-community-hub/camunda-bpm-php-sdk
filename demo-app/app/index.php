61bbbe19825320f207fe114984c038c88570be9e
Stefan Hentschel <stefan.hentschel@camunda.com>
2013-05-28 13:15:24 +0200

    fix(source): fix path for sources

--- demo-app/app/index.php
+++ demo-app/app/index.php
@@ -24,12 +24,12 @@ if(!$login->checkSession()) {
 <head>
   <title>camunda PHP incubation demo</title>
   <meta charset="UTF-8">
-  <link rel="stylesheet" type="text/css" href="../demo-app/assets/vendor/twitter/bootstrap/css/bootstrap.css" />
-	<link rel="stylesheet" type="text/css" href="../demo-app/assets/vendor/twitter/bootstrap/css/bootstrap-responsive.css" />
-	<link rel="stylesheet" type="text/css" href="../demo-app/assets/css/app.css" />
+  <link rel="stylesheet" type="text/css" href="../assets/vendor/twitter/bootstrap/css/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="../assets/vendor/twitter/bootstrap/css/bootstrap-responsive.css" />
+	<link rel="stylesheet" type="text/css" href="../assets/css/app.css" />
 
-	<script src="../demo-app/assets/vendor/jquery/js/jquery-1.9.1.js" language="javascript" type="text/javascript"></script>
-	<script src="../demo-app/assets/vendor/twitter/bootstrap/js/bootstrap.js" language="javascript" type="text/javascript"></script>
+	<script src="../assets/vendor/jquery/js/jquery-1.9.1.js" language="javascript" type="text/javascript"></script>
+	<script src="../assets/vendor/twitter/bootstrap/js/bootstrap.js" language="javascript" type="text/javascript"></script>
 </head>
 <body>
 	<header class="navbar navbar-fixed-top">

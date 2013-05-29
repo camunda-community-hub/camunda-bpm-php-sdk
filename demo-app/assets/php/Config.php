8ab99c179320116b9af52188d2286bf1bcb716c2
Stefan Hentschel <stefan.hentschel@camunda.com>
2013-05-29 15:38:28 +0200

    fix(config): add missing config

---
+++ demo-app/assets/php/Config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: hentschel
+ * Date: 27.05.13
+ * Time: 15:42
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace org\camunda\demo\php;
+
+
+class Config {
+  public static $applicationVersion = '0.0.1';
+  public static $isDemo = false;
+}
